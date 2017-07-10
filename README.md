93eafa5e9201714068a5518075d60dd91a4fba29
Vik Bhatti <vik@vikbhatti.com>
2017-07-10 12:47:31 +0100

    Initial project setup with pybuilder config, readme, and apache 2.0 license

---
+++ README.md
@@ -0,0 +1,17 @@
+Shippy - The docker builderoo
+=============================
+
+This is a simple project which demonstrates a few different ways to provision a docker image given a repo commit hash. Built using [pybuilder](http://pybuilder.github.com).
+
+
+# Dependencies
+
+
+
+# License
+
+Licensed under [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+
+# Author
+
+Vik Bhatti (github@vikbhatti.com)
---
+++ build.py
@@ -0,0 +1,57 @@
+#  shippy
+#  Copyright 2017 Vik Bhatti
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+"""
+A simple project that builds and deploys a docker container based on a repository SHA
+
+"""
+
+from pybuilder.core import use_plugin, init, Author
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("filter_resources")
+
+name = "shippy"
+authors = [Author("Vik Bhatti", "github@vikbhatti.com")]
+license = "Apache License, Version 2.0"
+url = "https://github.com/codesplicer/shippy"
+summary = "Application to build and deploy docker containers based on a repository commit hash"
+version = "0.1.0"
+
+default_task = ["clean", "install_dependencies", "analyze", "publish"]
+
+
+@init
+def set_properties(project):
+    project.build_depends_on("coverage")
+    # project.build_depends_on("")
+
+    project.set_property("name", "shippy")
+
+    # Set coverage properties
+    project.set_property("coverage_break_build", True)
+    project.set_property("coverage_threshold_warn", 70)
+
+    # Set linter properties
+    project.set_property("flake8_break_build", True)
+    project.set_property("flake8_ignore", "E501")   # Ignore lines longer than 80 chars
+    project.set_property("flake8_verbose_output", True)
+
+    project.get_property('filter_resources_glob').append('**/shippy/__init__.py')
